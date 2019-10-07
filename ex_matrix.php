4860907d3069aecca973328e5e037afa33cf1be3
=VQuang <vinhquangnguyen2@gmail.com>
2019-10-07 14:49:00 +0700

    Add CSS and RegEx

--- ex_matrix.php
+++ ex_matrix.php
@@ -3,26 +3,51 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Matrix</title>
+	<style>
+		body
+		{
+			background-color: #9176F6;
+		}
+		h2
+		{
+			color: red;
+		}
+		form
+        {
+        	background-color: #9176F6;        
+            margin: 0px;
+            width: 35%;
+            left: 35%;
+            position: absolute;
+        }
+	</style>
 </head>
 <body>
 	<?php
-		$str_indexMatrix = "";
 		$arr = array();
-		$m = 0;
-		$n = 0;
-		function splitStr($str)
+		$m= 0;
+		$n= 0;
+		$rowReq ="";
+		$colReq = "";
+		function checkValueOnly($str) //Kiểm tra chuỗi chỉ chứa kí tự số
 		{
-			$arr = explode(",", $str);
-			return $arr;
+			return preg_match('/^[0-9]+$/', $str);
 		}
-		function printMatrix($matrix, $m, $n)
+		function String2Value($str)
 		{
-			for($i = 0; $i < $m; $i++)
+			return intval($str);
+		}
+		function printMatrix($matrix) //Tạo bảng matrix gồm m hàng n cột
+		{
+			echo "<table>";
+			for($i = 0; $i < count($matrix); $i++)
 			{
-				for($j = 0; $j < $n; $j++)
-					echo $matrix[$i][$j]."\t\t\t";
-				echo "</br>";
+				echo "<tr>";
+				for($j = 0; $j < count($matrix[$i]); $j++)
+					echo "<td style='border: 3px solid #29F4DF; text-align: center;'>".$matrix[$i][$j]."</td>";
+				echo "</tr>";
 			}
+			echo "</table>";
 
 		}
 		function createMatrix($m, $n)
@@ -32,36 +57,64 @@
 			{
 				for($j = 0; $j < $n; $j++)
 				{
-					$matrix[$i][$j] = rand(-100,100);
+					$matrix[$i][$j] = rand(-100,100); //Tạo ngẫu nhiên giá trị từng phần tử trong matrix [-100;100]
 				}
 			}
-			printMatrix($matrix, $m, $n);
+			return $matrix;
 		} 
-		if(isset($_POST["indexMatrix"], $_POST["CreateMatrix"]))
+		if(isset($_POST["rowMatrix"],$_POST["colMatrix"], $_POST["CreateMatrix"]))
 		{
-			$str_indexMatrix = $_REQUEST["indexMatrix"];
-			if($str_indexMatrix != "")
+			if(checkValueOnly($_REQUEST["rowMatrix"]) && checkValueOnly($_REQUEST["colMatrix"]))
 			{
-				$arr = splitStr($str_indexMatrix);
-				$m = $arr[0];
-				$n = $arr[1];
+				$m = String2Value($_REQUEST["rowMatrix"]); //Chuyển chuỗi thành số
+				$n = String2Value($_REQUEST["colMatrix"]); //Chuyển chuỗi thành số
+				if($m > 10 || $m < 2 || $n > 10 || $n < 2) //Kiểm tra số hàng và số cột nằm trong khoảng [2;10]
+				{
+					if($m > 10 || $m < 2)
+						$rowReq = "Số hàng nằm trong khoảng [2;10]";
+					if($n > 10 || $n < 2)
+						$colReq = "Số cột nằm trong khoảng [2;10]";
+				}
+				else
+					$arr = createMatrix($m, $n);
 			}
+			else
+			{
+				if(!checkValueOnly($_REQUEST["rowMatrix"]))
+					$rowReq = "Chỉ chấp nhận số!";
+				if(!checkValueOnly($_REQUEST["colMatrix"]))
+					$colReq = "Chỉ chấp nhận số!";
+			}
+
 		}
 
 	?>
 	<form action="" method="POST">
-		<table>
-			<h3>Ma Trận</h3>
+		<h2 align="center">MA TRẬN</h2>
+		<table border="0">
 			<tr>
-				<td>Nhập m,n: </td>
-				<td><input type="text" name="indexMatrix" value="<?php echo $m.",".$n ?>"> (*)</td>
+				<td>Nhập số hàng: </td>
+				<td><input type="text" name="rowMatrix" value="<?php echo $m ?>"> (*) </td>
 			</tr>
 			<tr>
+				<td></td>
+				<td><?php echo $rowReq?></td>
+			</tr>
+			<tr>
+				<td>Nhập số cột: </td>
+				<td><input type="text" name="colMatrix" value="<?php echo $n ?>"> (*) </td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><?php echo $colReq?></td>
+			</tr>
+			<tr>
+				<td></td>
 				<td><input type="submit" name="CreateMatrix" value="Tạo ma trận"></td>
 			</tr>
 			<tr>
 				<td>Ma trận: </td>
-				<td><?php createMatrix($m, $n) ?></td>
+				<td><?php printMatrix($arr) ?></td>
 			</tr>
 		</table>
 	</form>
